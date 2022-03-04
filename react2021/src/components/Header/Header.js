dd5587b1405c4227e548ec6fddf4f4764893bd48
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-04 23:07:47 +0100

    ps6-zad6

--- react2021/src/components/Header/Header.js
+++ react2021/src/components/Header/Header.js
@@ -3,8 +3,10 @@ import styles from '../../common/styles/Headers.module.scss';
 import { Link } from 'react-router-dom';
 
 function Header() {
+    const currentUserName = JSON.parse(window.localStorage.getItem('user'));
     return (
         <div className={styles.Wrapper}>
+            <p>Logged user: {currentUserName}</p>
             <Link to='/'>
                 <button>Sign out</button>
             </Link>
--- react2021/src/components/LoginPage/LoginPage.js
+++ react2021/src/components/LoginPage/LoginPage.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
 function LoginPage() {
+    let navigate = useNavigate();
+
+    const [userName, setUserName] = useState('');
+
+    const signInUser = (e) => {
+        e.preventDefault();
+        window.localStorage.setItem('user', JSON.stringify(userName))
+        navigate('/shopping')
+    }
     return (
         <div>
-            <Link to="/shopping" >
-                <button> Sign in</button>
-            </Link>
+            <form onSubmit={signInUser}>
+                <label>User name</label>
+                <input onChange={(event) => setUserName(event.target.value)}></input>
+                <button type='submit'> Sign in</button>
+            </form>
         </div>
       );
   };
