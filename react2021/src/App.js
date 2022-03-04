f2167af99abd38683629a3fd7e147ba1b4a7c715
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-04 23:16:19 +0100

    ps6-zad7

--- react2021/src/App.js
+++ react2021/src/App.js
@@ -3,8 +3,16 @@ import ShopingList from './components/ShopingList/ShopingList';
 import Header from './components/Header/Header';
 // import ProductsFilters from './components/ProductsFilters/ProductsFilters';
 import styles from './App.module.scss';
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const userExist = localStorage.getItem('user');
+
+  if (!userExist) {
+    return (
+      <Navigate to="/" />
+    )
+  }
   return (
     <div className={styles.appWrapper}>
       <Header />
