35093d362ce64ef23786da174d170cf2f41e5c0a
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-04 22:32:09 +0100

    ps6-zad5

--- react2021/src/components/Header/Header.js
+++ react2021/src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from '../../common/styles/Headers.module.scss';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className={styles.Wrapper}>
-            Header
+            <Link to='/'>
+                <button>Sign out</button>
+            </Link>
         </div>
       );
   };
--- react2021/src/components/LoginPage/LoginPage.js
+++ react2021/src/components/LoginPage/LoginPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function LoginPage() {
     return (
         <div>
-            <button> Sign in</button>
+            <Link to="/shopping" >
+                <button> Sign in</button>
+            </Link>
         </div>
       );
   };
