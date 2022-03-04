729cc43773d733a0d23c19d9382c39e96ca1dec3
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-04 22:24:52 +0100

    ps6-zad3

---
+++ react2021/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div>
+            <button> Sign in</button>
+        </div>
+      );
+  };
+
+  export default LoginPage;
--- react2021/src/index.js
+++ react2021/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App';
+// import App from './App';
+import LoginPage from './components/LoginPage/LoginPage';
 // import ProductsList from './components/ProductsList/ProductsList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-    <Route path="/" element={<App />} />
+    <Route path="/" element={<LoginPage />} />
   </Routes>
   </BrowserRouter>,
   document.getElementById('root')
