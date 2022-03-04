3cf3af1f12816ebe6f4b8e19e382a5e47b292ebd
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-04 22:27:08 +0100

    ps6-zad4

--- react2021/src/index.js
+++ react2021/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import App from './App';
+import App from './App';
 import LoginPage from './components/LoginPage/LoginPage';
 // import ProductsList from './components/ProductsList/ProductsList';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +11,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
     <Route path="/" element={<LoginPage />} />
+    <Route path="/shopping" element={<App />} />
   </Routes>
   </BrowserRouter>,
   document.getElementById('root')
