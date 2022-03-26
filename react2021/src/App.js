2847ec0725a0293709194ede13b456e5a47756dc
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-26 21:38:05 +0100

    ps7-zad1

--- react2021/src/App.js
+++ react2021/src/App.js
@@ -3,9 +3,14 @@ import Header from './components/Header/Header';
 import styles from './App.module.scss';
 import { Navigate } from "react-router-dom";
 import airports from './common/consts/airports';
+import { useEffect } from 'react';
 
 function App() {
   const userExist = localStorage.getItem('user');
+
+  useEffect(() => {
+    window.localStorage.setItem('airports', JSON.stringify(airports));
+  }, [])
 
   if (!userExist) {
     return (
--- react2021/src/components/AirportsList/AirportsList.js
+++ react2021/src/components/AirportsList/AirportsList.js
@@ -2,12 +2,13 @@ import React from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 
 function ProductsList(props) {
+  const airportsList = JSON.parse(window.localStorage.getItem('airports'));
 
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <p>Lista lotnisk</p>
-        {props.airports.map((airport) => <span> {airport.name} </span>)}
+        {airportsList.map((airport) => <span> {airport.name} </span>)}
       </header>
     </div>
   );
