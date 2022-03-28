bfbab4fab25e52a3202951dbd1a3af6887cf3dfa
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-28 22:43:59 +0200

    ps7-zad8

--- react2021/src/components/AirportDetails/AirportDetails.js
+++ react2021/src/components/AirportDetails/AirportDetails.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 import { useParams, useNavigate } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Button } from "@mui/material";
+
 
 function AirportDetails(props) {
   let { id } = useParams();
   let navigate = useNavigate();
+
   console.log('id', id)
   const airportDetails = JSON.parse(localStorage.getItem('airports')).find((airport) => airport.id === id);
   console.log('airportDetails', airportDetails)
@@ -16,10 +24,27 @@ function AirportDetails(props) {
     window.localStorage.setItem('airports', JSON.stringify(listWithoutCurrentAirport));
     navigate(-1);
   }
+
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
+    console.log('dialogIsOpen', dialogIsOpen)
   return (
     <>
+        <Dialog
+    open={dialogIsOpen}
+  >
+    <DialogTitle>
+      Czy na pewno chcesz usunąć lotnisko
+    </DialogTitle>
+    <DialogActions>
+      <Button onClick={() => setDialogIsOpen(false)}>Nie</Button>
+      <Button onClick={() => handleRemoveAirportFromList()} autoFocus>
+        Tak
+      </Button>
+    </DialogActions>
+  </Dialog>
     <ArrowBackIcon onClick={() => navigate(-1)} fontSize="large" />
-    <DeleteForeverIcon onClick={handleRemoveAirportFromList} fontSize="large" />
+    <DeleteForeverIcon onClick={() => setDialogIsOpen(true)} fontSize="large" />
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <p>Airport Details</p>
--- react2021/src/components/AirportsList/AirportsList.js
+++ react2021/src/components/AirportsList/AirportsList.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 import { Link } from "react-router-dom";
 import { Stack, Paper } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function ProductsList(props) {
   let airportsList = JSON.parse(window.localStorage.getItem('airports'));
