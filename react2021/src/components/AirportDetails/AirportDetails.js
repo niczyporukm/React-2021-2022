532a9c321b53a5db77da0d65d7316098ba08d949
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-03-28 21:34:22 +0200

    ps7-zad4

--- react2021/src/components/AirportDetails/AirportDetails.js
+++ react2021/src/components/AirportDetails/AirportDetails.js
@@ -1,12 +1,20 @@
 import React from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
+import { useParams } from 'react-router-dom';
 
 function AirportDetails(props) {
-
+  let { id } = useParams();
+  console.log('id', id)
+  const airportDetails = JSON.parse(localStorage.getItem('airports')).find((airport) => airport.id === id);
+  console.log('airportDetails', airportDetails)
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <p>Airport Details</p>
+        <span>Państwo: {airportDetails.country}</span>
+        <span>Miasto: {airportDetails.city}</span>
+        <span>Nazwa: {airportDetails.name}</span>
+        <span>KOD: {airportDetails.iata_code}</span>
       </header>
     </div>
   );
